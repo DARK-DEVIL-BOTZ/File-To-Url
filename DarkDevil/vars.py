c1908c0f298e95b6658e77ace83879250e319555
TEAM DARK DEVIL <111672563+DARK-DEVIL-BOTZ@users.noreply.github.com>
2022-08-25 20:19:51 +0530

    💡DARK-DEVIL-BOTZ™️🇱🇰

--- DarkDevil/vars.py
+++ DarkDevil/vars.py
@@ -3,36 +3,38 @@ import os
 from os import getenv, environ
 from dotenv import load_dotenv
 
+
+
 load_dotenv()
 
 class Var(object):
-   MULTI_CLIENT = False
-    API_ID = (19562731)
-    API_HASH = (1667473e722f17eaa8ccbb98dc5f727d)
-    BOT_TOKEN = (5505255276:AAFi2EviEpXs3cppv1EQscKQvGVl-hcShLg)
-    SESSION_NAME = (file-to-url)
-    SLEEP_THRESHOLD = (60)
-    WORKERS = (4)
-    BIN_CHANNEL = (-1001756916904)
-    PORT = (8080)
-    BIND_ADRESS = (0.0.0.0)
-    PING_INTERVAL = (1200)
-    OWNER_ID = (2071644540)
-    NO_PORT = False
+    MULTI_CLIENT = False
+    API_ID = int(getenv('API_ID'))
+    API_HASH = str(getenv('API_HASH'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN'))
+    SESSION_NAME = str(getenv('SESSION_NAME', 'filetolinkbot'))
+    SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
+    WORKERS = int(getenv('WORKERS', '4'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
+    PORT = int(getenv('PORT', 8080))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())  
+    NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = (DARKDevilV2)
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
     
     else:
         ON_HEROKU = False
-    FQDN = FQDN', BIND_ADRESS)) if not ON_HEROKU
-    else APP_NAME+'.herokuapp.com'
-    HAS_SSL=bool
+    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+    HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = (mongodb+srv://malindunimsara2:darkdevil7@cluster0.q6m3t.mongodb.net/?retryWrites=true&w=majority)
-    UPDATES_CHANNEL = (DarkDevilBotz)
+    DATABASE_URL = str(getenv('DATABASE_URL'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
