1224f2bfccb867380945901d26d56f997779b6cf
TEAM DARK DEVIL <111672563+DARK-DEVIL-BOTZ@users.noreply.github.com>
2022-08-25 17:35:46 +0530

    💡DARK-DEVIL-BOTZ™️🇱🇰

--- DarkDevil/__main__.py
+++ DarkDevil/__main__.py
@@ -12,7 +12,7 @@ from .vars import Var
 from aiohttp import web
 from .server import web_server
 from .utils.keepalive import ping_server
-from Adarsh.bot.clients import initialize_clients
+from DarkDevil.bot.clients import initialize_clients
 
 logging.basicConfig(
     level=logging.INFO,
@@ -22,7 +22,7 @@ logging.getLogger("aiohttp").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
 
-ppath = "Adarsh/bot/plugins/*.py"
+ppath = "DarkDevil/bot/plugins/*.py"
 files = glob.glob(ppath)
 StreamBot.start()
 loop = asyncio.get_event_loop()
@@ -46,12 +46,12 @@ async def start_services():
         with open(name) as a:
             patt = Path(a.name)
             plugin_name = patt.stem.replace(".py", "")
-            plugins_dir = Path(f"Adarsh/bot/plugins/{plugin_name}.py")
+            plugins_dir = Path(f"DarkDevil/bot/plugins/{plugin_name}.py")
             import_path = ".plugins.{}".format(plugin_name)
             spec = importlib.util.spec_from_file_location(import_path, plugins_dir)
             load = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(load)
-            sys.modules["Adarsh.bot.plugins." + plugin_name] = load
+            sys.modules["DarkDevil.bot.plugins." + plugin_name] = load
             print("Imported => " + plugin_name)
     if Var.ON_HEROKU:
         print("------------------ Starting Keep Alive Service ------------------")
